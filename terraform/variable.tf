9230c4f297da4ec2b911fcb36410ad2ee900f030
Ananya Aggarwal <68246207+ananya39@users.noreply.github.com>
2025-04-27 14:23:42 +0530

    Update variable.tf

--- terraform/variable.tf
+++ terraform/variable.tf
@@ -10,13 +10,5 @@ variable "key_name" {
   description = "SSH key pair name to access the instance"
   default="strapinew"
 }
-variable "AWS_ACCESS_KEY_ID" {
-  description = "AWS Access Key ID"
-  type        = string
-}
 
-variable "AWS_SECRET_ACCESS_KEY" {
-  description = "AWS Secret Access Key"
-  type        = string
-}
 
